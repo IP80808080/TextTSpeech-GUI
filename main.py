d17d8d51f07ddcc1ff9fbe4b9acce965216ad91f
KamleshKasambe <kasambe.kamlesh1@gmail.com>
2023-05-13 13:35:07 +0530

    GUI based with Tkinter to convert Text to Speech.

---
+++ main.py
@@ -0,0 +1,121 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import filedialog
+from tkinter.ttk import Combobox
+import pyttsx3
+import os
+
+root =Tk()
+root.title("Text to Speech")
+root.geometry("900x450+200+200")
+root.resizable(True,True)
+root.configure(bg='#305065')
+
+engine = pyttsx3.init()
+
+# for text to speak
+
+def speaknow():
+    text = text_area.get(1.0, END)
+    gender = gender_combobox.get()
+    speed = speed_combobox.get()
+    voices = engine.getProperty('voices')
+
+    def setvoices():
+        if gender == 'Male':
+            engine.setProperty('voice', voices[0].id)
+            engine.say(text)
+            engine.runAndWait()
+        else:
+            engine.setProperty('voice', voices[1].id)
+            engine.say(text)
+            engine.runAndWait()
+
+    if (text):
+        if (speed=='Fast'):
+            engine.setProperty('rate', 250)
+            setvoices()
+        elif (speed == 'Normal'):
+            engine.setProperty('rate', 150)
+            setvoices()
+        else:
+            engine.setProperty('rate', 60)
+            setvoices()
+
+
+#Download
+
+def download():
+    text = text_area.get(1.0, END)
+    gender = gender_combobox.get()
+    speed = speed_combobox.get()
+    voices = engine.getProperty('voices')
+
+    def setvoices():
+        if gender == 'Male':
+            engine.setProperty('voice', voices[0].id)
+            path = filedialog.askdirectory()
+            os.chdir(path)
+            engine.save_to_file(text, 'text.mp3')
+            engine.runAndWait()
+        else:
+            engine.setProperty('voice', voices[1].id)
+            path = filedialog.askdirectory()
+            os.chdir(path)
+            engine.save_to_file(text, 'text.mp3')
+            engine.runAndWait()
+
+    if (text):
+        if (speed=='Fast'):
+            engine.setProperty('rate', 250)
+            setvoices()
+        elif (speed == 'Normal'):
+            engine.setProperty('rate', 150)
+            setvoices()
+        else:
+            engine.setProperty('rate', 60)
+            setvoices()
+
+#icon
+
+image_icon = PhotoImage(file="K:/Projects/Programming/Python/Extra/Material/Image_Assests/Resize/Mic-1.png")
+root.iconphoto(False, image_icon)
+
+#Top Frame
+
+Top_Frame = Frame(root, bg='white', width=900, height=100)
+Top_Frame.place(x=0, y=0)
+
+Logo = PhotoImage(file="K:/Projects/Programming/Python/Extra/Material/Image_Assests/Resize/Mic-1.png")
+Label(Top_Frame, image=Logo, bg='white').place(x=10,y=5)
+
+Label(Top_Frame, text='TEXT TO SPEECH', font='TimesNewroman 20 bold', bg='white', fg='black').place(x=100, y=30)
+
+################
+
+text_area = Text(root,font='TimesNewroman 20', bg='white', relief=GROOVE, wrap=WORD)
+text_area.place(x=10, y=150, width=500, height=250)
+
+Label(root, text="VOICE", font='TimesNewroman 15 bold', bg='#305065', fg='white').place(x=580, y=160)
+Label(root, text="Speed", font='TimesNewroman 15 bold', bg='#305065', fg='white').place(x=760, y=160)
+
+gender_combobox = Combobox(root, values=['Male', 'Female'], font='TimesNewroman 14', state='r', width=10)
+gender_combobox.place(x=550, y=200)
+gender_combobox.set('Male')
+
+speed_combobox = Combobox(root, values=['Fast', 'Normal', 'Slow'], font='TimesNewroman 14', state='r', width=10)
+speed_combobox.place(x=730, y=200)
+speed_combobox.set('Male')
+speed_combobox.set('Normal')
+
+#Button
+
+imageicon = PhotoImage(file="K:/Projects/Programming/Python/Extra/Material/Image_Assests/Resize/Listen-1.png")
+btn= Button(root, text="Speak", compound=LEFT, image=imageicon, width=130, font='TimesNewroman 14 bold', command=speaknow)
+btn.place(x=550, y=280)
+
+imageicon2 = PhotoImage(file="K:/Projects/Programming/Python/Extra/Material/Image_Assests/Resize/Download-1.png")
+save= Button(root, text="Save", compound=LEFT, image=imageicon2, width=130, bg='#39c790', font='TimesNewroman 14 bold', command=download)
+save.place(x=730, y=280)
+
+root.mainloop()
